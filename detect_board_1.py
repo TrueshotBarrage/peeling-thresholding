b3fbb85b51d02713a32734863289e34f71102567
Daniel Stabile <cosmos@dhcp-vl2042-5792.redrover.cornell.edu>
2022-06-09 14:32:37 -0400

    refactored code in peeling_color for readability.  Moved old files to archive

---
+++ detect_board_1.py
@@ -0,0 +1,125 @@
+from __future__ import print_function
+from nis import match
+import cv2 as cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import argparse
+import os
+
+# Parse arguments
+parser = argparse.ArgumentParser(description='Code for detecting peeled areas in vegetables')
+parser.add_argument('--folder', help='Path to input images.', default='potato')
+parser.add_argument('--image', help='Name of image to process. If empty all images are processed.', default='')
+args = parser.parse_args()
+
+# Iterate through folder or select specific image
+path = os.path.join("./img_data/", args.folder)
+images =  next(os.walk(path), (None, None, []))[2] if args.image == '' else [args.image]
+
+# Loop through images and process
+for imFile in sorted(images):
+
+    # Load image and convert to gray
+    print(f"Processing {imFile }")
+    imPath = os.path.join(path, imFile)
+    img = cv2.imread(imPath) 
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    if img is None:
+        print('Could not open or find the images!')
+        exit(0)
+
+    ########## Process image ########## 
+
+    
+
+    # Initiate SIFT detector
+    sift = cv2.SIFT_create()
+
+    # Find the keypoints and descriptors with SIFT
+    kp1, des1 = sift.detectAndCompute(gray1,None)
+    kp2, des2 = sift.detectAndCompute(gray2,None)
+
+    # FLANN parameters
+    FLANN_INDEX_KDTREE = 0
+    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+    search_params = dict(checks=50)   # or pass empty dictionary
+
+    # Get matches
+    flann = cv2.FlannBasedMatcher(index_params,search_params)
+    matches = flann.knnMatch(des1,des2,k=2)
+    print(f"Matches: {len(matches)}")
+
+    # Need to draw only good matches, so create a mask
+    matchesMask = [[0,0] for i in range(len(matches))]
+    goodMatches = []
+
+    # Ratio test as per Lowe's paper
+    ratioThresh = 1
+    for i,(m,n) in enumerate(matches):
+        if m.distance < ratioThresh * n.distance:
+            matchesMask[i]=[1,0]
+            goodMatches.append(m)
+
+    draw_params = dict(matchColor = (0,255,0),
+                    singlePointColor = (255,0,0),
+                    matchesMask = matchesMask,
+                    flags = 0)
+
+    img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,matches,None,**draw_params)
+
+    # -------- Draw bounding box ---------
+    import math
+
+    def rotate(origin, point, angle):
+        """
+        Rotate a point counterclockwise by a given angle around a given origin.
+
+        The angle should be given in radians.
+        """
+        ox, oy = origin
+        px, py = point
+
+        qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
+        qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
+        return qx, qy
+
+    # Find top two matches
+    goodMatches = sorted(goodMatches, key = lambda x:x.distance)
+    goodMatches = goodMatches[:2]
+    print(f'Good Matches: {goodMatches}')
+
+    # Get angles between top two points in query and train images
+    queryPt1 = kp1[goodMatches[0].queryIdx].pt 
+    queryPt2 = kp1[goodMatches[1].queryIdx].pt
+    trainPt1 = kp2[goodMatches[0].trainIdx].pt
+    trainPt2 = kp2[goodMatches[1].trainIdx].pt
+    queryAngle =  np.arctan2(queryPt2[0] - queryPt1[0], queryPt2[1] - queryPt1[1])
+    print(f'Query Angle: {queryAngle}')
+    trainAngle =  np.arctan2(trainPt2[0] - trainPt1[0], trainPt2[1] - trainPt1[1])
+    print(f'Train Angle: {trainAngle}')
+    AngleDiff =  queryAngle - trainAngle
+
+    # Find center of query image in relation to top two query points
+    h, w = img1.shape[:2]
+    queryCenter = [w//2, h//2]
+    print(f"Height: {h} Width: {w} Center: {queryCenter}")
+
+    queryOffset = [queryCenter[0] - queryPt1[0] , queryCenter[1] - queryPt1[1]]
+    print(f'Query Offset: {queryOffset}')
+
+    # Find center in train image using top train point and angle difference
+    trainOffset = rotate([0,0], queryOffset, AngleDiff)
+    trainCenter = [int(trainPt1[0] + trainOffset[0]), int(trainPt1[1] + trainOffset[1])]
+    print(f"Train Center: {trainCenter}")
+
+    # Draw center
+    img3 = cv2.circle(img3, [trainCenter[0]  + w,    trainCenter[1]],  10, (255, 0, 0), -1)
+    img3 = cv2.circle(img3, [int(trainPt1[0] + w), int(trainPt1[1])],  5, (0, 0, 255), 2)
+    img3 = cv2.circle(img3, [int(trainPt2[0] + w), int(trainPt2[1])],  5, (0, 0, 0), 2)
+    img3 = cv2.circle(img3, queryCenter,                              10, (255, 0, 0), -1)
+    img3 = cv2.circle(img3, [int(queryPt1[0]),     int(queryPt1[1])],  5, (0, 0, 255), 2)
+    img3 = cv2.circle(img3, [int(queryPt2[0]),     int(queryPt2[1])],  5, (0, 0, 0), 2)
+
+    plt.imshow(img3,),plt.show()
+
+ 
--- peeling_color.py
+++ peeling_color.py
@@ -32,14 +32,14 @@ for imFile in sorted(images):
     ###########  Process image ###########
 
     # Crop sides
-    h, w, c = img.shape
-    minW, maxW = int(w/10), int(9*w/10)
-    print(f"Min: {minW} Max: {maxW}")
+    # h, w, c = img.shape
+    # minW, maxW = int(w/10), int(9*w/10)
+    # print(f"Min: {minW} Max: {maxW}")
     # img = img[:, minW:maxW]
-    print(img.shape)
+    # print(img.shape)
 
     # Normalize image to min and max intensity of 0 and 255
-    print(f"Crop min: {np.min(img)} max: {np.max(img)}")
+    print(f"Original min: {np.min(img)} max: {np.max(img)}")
     norm = np.zeros_like(img)
     norm = cv2.normalize(img, norm, 0, 255, cv2.NORM_MINMAX)
     print(f"Norm min: {np.min(norm)} max: {np.max(norm)}")
@@ -53,7 +53,17 @@ for imFile in sorted(images):
     blue  = cv2.merge([zeros, zeros, B])
     gray = cv2.cvtColor(norm, cv2.COLOR_BGR2GRAY)
 
-    def segment(img):
+    """
+    Threshold grayscale image using the following three methods:
+        1. Binary thresholding with threshold = 127
+        2. Otsu method
+        3. Gaussian filtering + Otsu method
+
+    Input: Grayscale image
+    Output: Three binary images, respectively processed by the three methods 
+        described above
+    """
+    def threshold(img):
         # global thresholding
         ret1,th1 = cv2.threshold(img, 127,255, cv2.THRESH_BINARY)
         # Otsu's thresholding
@@ -63,19 +73,26 @@ for imFile in sorted(images):
         ret3,th3 = cv2.threshold(blur,0,255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
         return th1, th2, th3
 
-    i1, i2, i3 = segment(gray)
-    r1, r2, r3 = segment(R)
-    g1, g2, g3 = segment(G)
-    b1, b2, b3 = segment(B)
+    # Roughly segment vegetable in image using thresholding
+    # Will refine later
+    imgThresh1, imgThresh2, imgThresh3 = threshold(gray)
+    redThresh1, redThresh2, redThresh3 = threshold(R)
+    greenThresh1, greenThresh2, greenThresh3 = threshold(G)
+    blueThresh1, blueThresh2, blueThresh3 = threshold(B)
 
     # Display
     titles = ['Original', 'Red', 'Green', 'Blue', 
               'Original - Gray', 'Red - Gray', 'Green - Gray', 'Blue - Gray',
               'Original - Global', 'Red - Global', 'Green - Global', 'Blue - Global', 
               'Original - Otsu', 'Red - Otsu', 'Green - Otsu', 'Blue - Otsu', 
-              'Original - Otsu + Gauss', 'Red - Otsu + Gauss', 'Green - Otsu + Gauss', 'Blue - Otsu + Gauss']
-    images = [img, red, green, blue, gray, R, G, B, i1, r1, g1, b1, i2, r2, g2, b2, i3, r3, g3, b3]
-    # images = [img, gray, R, G, B]
+              'Original - Otsu + Gauss', 'Red - Otsu + Gauss', 'Green - Otsu + Gauss', 'Blue - Otsu + Gauss',
+              ]
+    images = [img, red, green, blue, 
+            gray, R, G, B, 
+            imgThresh1, redThresh1, greenThresh1, blueThresh1, 
+            imgThresh2, redThresh2, greenThresh2, blueThresh2, 
+            imgThresh3, redThresh3, greenThresh3, blueThresh3,
+            ]
 
     # Display image
     for i in range(20):
@@ -85,9 +102,20 @@ for imFile in sorted(images):
     plt.show()
 
     ###### Segment #######
-    # Use blue channel as it seems to provide most accurate segmentations
-    # Of the three methods, find the one where the masked area is smallest as to not include bleed
+    # Refine previous segmentation to get outline of entire vegetable and 
+    # separate segmentation of peeled area.
+    # Use blue channel as it seems to provide most accurate segmentations.
+    # Of the three methods, find the one where the masked area is smallest as to 
+    # not include bleed.
 
+    """
+    Perform morphology to remove isolated black dots surrounding vegetable and 
+        close white areas within the vegetable
+
+    Input: Binary image
+    Output: Binary image that has been closed and then opened.  Sorry for the 
+        bad documentation lol
+    """
     def morph(img):
         kernel = np.ones((5,5),np.uint8)
         img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
@@ -95,80 +123,76 @@ for imFile in sorted(images):
         img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
         return img
 
+    # Use morphology to remove isolated black spots and remove white openings in vegetable
+    morph1 = morph(blueThresh1)
+    morph2 = morph(blueThresh2)
+    morph3 = morph(blueThresh3)
 
-    clean1 = morph(b1)
-    clean2 = morph(b2)
-    clean3 = morph(b3)
+    # Get copies to draw on
+    imgDraw1, imgDraw2, imgDraw3 = np.copy(img), np.copy(img), np.copy(img)
 
-    img1, img2, img3 = np.copy(img), np.copy(img), np.copy(img)
+    """
+    Finds contours in imgBinary and draws outline of first contour on imgDraw
+    in green and creates binary mask of first contour.  Both images get cropped
+    size of first contour
+    
+    Input: 
+        imgDraw - RGB image
+        imgBinary - Binary image
+    
+    Output: 
+        imgDraw - RGB image with outline of first contour found drawn in green.
+            Cropped to size of first contour.
+        mask - Binary image of first contour found filled with white
+            Cropped to size of first contour.
+    """
+    def findDrawFirstContour(imgDraw, imgBinary):
+        # Get mask
+        contours, hierarchy = cv2.findContours(np.invert(imgBinary), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        print(f'Number of contours: {len(contours)}')
+        cv2.drawContours(imgDraw,    contours, 0, (0, 255, 0), 3)
+        mask = np.zeros_like(imgDraw[:,:,0])
+        mask =  cv2.drawContours(mask, contours, 0, (255), -1)
+        # get the bounding rect
+        x, y, w, h = cv2.boundingRect(contours[0])
+        # to save the images
+        imgDraw = imgDraw[y:y+h,x:x+w]
+        mask= mask[y:y+h,x:x+w]
 
-    # Get mask
-    ret, thresh = cv2.threshold(clean1, 127, 255, 0)
-    contours, hierarchy = cv2.findContours(np.invert(thresh), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    print(f'Number of contours: {len(contours)}')
-    cv2.drawContours(img1,    contours, 0, (0, 255, 0), 3)
-    mask1 = np.zeros_like(img1[:,:,0])
-    mask1 =  cv2.drawContours(mask1, contours, 0, (255), -1)
-    # get the bounding rect
-    x, y, w, h = cv2.boundingRect(contours[0])
-    # to save the images
-    img1 = img1[y:y+h,x:x+w]
-    mask1 = mask1[y:y+h,x:x+w]
+        return mask, imgDraw
 
+    # Get masks and draw vegetable outline
+    vegMask1, imgDraw1 = findDrawFirstContour(imgDraw1, morph1)
+    vegMask2, imgDraw2 = findDrawFirstContour(imgDraw2, morph2)
+    vegMask3, imgDraw3 = findDrawFirstContour(imgDraw3, morph3)
 
-    ret, thresh = cv2.threshold(clean2, 127, 255, 0)
-    contours, hierarchy = cv2.findContours(np.invert(thresh), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    print(f'Number of contours: {len(contours)}')
-    cv2.drawContours(img2,    contours, 0, (0, 255, 0), 5)
-    mask2 = np.zeros_like(img2[:,:,0])
-    mask2 =  cv2.drawContours(mask2, contours, 0, (255),-1)
-    # get the bounding rect
-    x, y, w, h = cv2.boundingRect(contours[0])
-    # to save the images
-    img2 = img2[y:y+h,x:x+w]
-    mask2 = mask2[y:y+h,x:x+w]
+    # Threshold different images usings the same method that the binary images
+    # fed into findDrawFirstContour were used to find the contour drawn on them
+    # Sorry if that's confusing!
+    thresh1, _, _ = threshold(cv2.cvtColor(imgDraw1, cv2.COLOR_BGR2GRAY))
+    _, thresh2, _ = threshold(cv2.cvtColor(imgDraw2, cv2.COLOR_BGR2GRAY))
+    _, _, thresh3 = threshold(cv2.cvtColor(imgDraw3, cv2.COLOR_BGR2GRAY))
 
-    ret, thresh = cv2.threshold(clean3, 127, 255, 0)
-    contours, hierarchy = cv2.findContours(np.invert(thresh), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    print(f'Number of contours: {len(contours)}')
-    cv2.drawContours(img3,    contours, 0, (0, 255, 0), 5)
-    mask3 = np.zeros_like(img3[:,:,0])
-    mask3 =  cv2.drawContours(mask3, contours, 0, (255),-1)
-    # get the bounding rect
-    x, y, w, h = cv2.boundingRect(contours[0])
-    # to save the images
-    img3 = img3[y:y+h,x:x+w]
-    mask3 = mask3[y:y+h,x:x+w]
+    # Get peel masks.  First erode the vegetable masks and then bitwise_and with
+    # thersholded images found right above.  Erosion is necessary as we don't
+    # want to include the contour of the vegetable which will be included in the 
+    # thresholded images above since the images fed into threshold() include the
+    # vegetable contours
+    def findDrawOverlap(imgDraw, vegMask, imgThresh):
+        kernel = np.ones((10,10),np.uint8)
+        # Erode imgMask to eliminate outline of mask when we bitwise_and
+        vegMask = cv2.erode(vegMask,kernel,iterations = 1)
+        overlap = cv2.bitwise_and(vegMask, imgThresh)
+        contours, hierarchy = cv2.findContours(overlap, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        print(f'Number of contours: {len(contours)}')
+        # contours = contours[2:] if len(contours) > 2 else [] 
+        print(len(contours))
+        cv2.drawContours(imgDraw,    contours, -1, (255, 0, 0), -1)
+        return overlap
 
-    thresh1, _, _ = segment(cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY))
-    _, thresh2, _ = segment(cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY))
-    _, _, thresh3 = segment(cv2.cvtColor(img3, cv2.COLOR_BGR2GRAY))
-
-    kernel = np.ones((10,10),np.uint8)
-    mask1 = cv2.erode(mask1,kernel,iterations = 1)
-    mask2 = cv2.erode(mask2,kernel,iterations = 1)
-    mask3 = cv2.erode(mask3,kernel,iterations = 1)
-    thresh1 = cv2.bitwise_and(mask1, thresh1)
-    thresh2 = cv2.bitwise_and(mask2, thresh2)
-    thresh3 = cv2.bitwise_and(mask3, thresh3)
-
-    contours, hierarchy = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    print(f'Number of contours: {len(contours)}')
-    # contours = contours[2:] if len(contours) > 2 else [] 
-    print(len(contours))
-    cv2.drawContours(img1,    contours, -1, (255, 0, 0), -1)
-
-    contours, hierarchy = cv2.findContours(thresh2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    print(f'Number of contours: {len(contours)}')
-    # contours = contours[2:] if len(contours) > 2 else [] 
-    print(len(contours))
-    cv2.drawContours(img2,    contours, -1, (255, 0, 0), -1)
-
-    contours, hierarchy = cv2.findContours(thresh3, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    print(f'Number of contours: {len(contours)}')
-    # contours = contours[2:] if len(contours) > 2 else [] 
-    print(len(contours))
-    cv2.drawContours(img3,    contours, -1, (255, 0, 0), -1)
+    peelMask1 = findDrawOverlap(imgDraw1, vegMask1, thresh1)
+    peelMask2 = findDrawOverlap(imgDraw2, vegMask2, thresh2)
+    peelMask3 = findDrawOverlap(imgDraw3, vegMask3, thresh3)
 
     # Display
     titles = ['Blue - Global Threshold', 'Blue - Otsu', 'Blue - Otsu + Threshold', 
@@ -177,11 +201,11 @@ for imFile in sorted(images):
               'Vegetable Mask',  'Vegetable Mask',  'Vegetable Mask',  
               'Peeled Mask',  'Peeled Mask',  'Peeled Mask']  
 
-    images = [b1, b2, b3, 
-              clean1, clean2, clean3, 
-              img1, img2, img3, 
-              mask1, mask2, mask3, 
-              thresh1, thresh2, thresh3]
+    images = [blueThresh1, blueThresh2, blueThresh3, 
+              morph1, morph2, morph3, 
+              imgDraw1, imgDraw2, imgDraw3, 
+              vegMask1, vegMask2, vegMask3, 
+              peelMask1, peelMask2, peelMask3]
 
     # Display image
     for i in range(15):
